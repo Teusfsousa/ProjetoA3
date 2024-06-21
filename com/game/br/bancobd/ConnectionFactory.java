4e39d715a557f8cc4ce64bcb8ca45bdb2b33863c
Mateus Fernandes <mateusfdesousa12@gmail.com>
2024-06-21 00:16:45 -0300

    Ajsutes no banco de dados, pois estava com erro de uso, o usuario não era encontrado no banco de dados.

--- com/game/br/bancobd/ConnectionFactory.java
+++ com/game/br/bancobd/ConnectionFactory.java
@@ -1,20 +1,23 @@
 package com.game.br.bancobd;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class ConnectionFactory {
-    private static final String DRIVE = "com.mysql.jdbc.Driver";
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver"; // Atualizado para o driver correto
     private static final String URL = "jdbc:mysql://localhost:3306/dbnumberguess";
     private static final String USER = "root";
     private static final String PASS = "1323@";
 
     public static Connection getConnection() {
         try {
-            return DriverManager.getConnection(URL, USER, PASS);
+            Class.forName(DRIVER); // Carrega o driver do banco de dados
+            return DriverManager.getConnection(URL, USER, PASS); // Cria e retorna a conexão
         } catch (SQLException e) {
             throw new RuntimeException("Erro na conexão", e);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Driver do banco de dados não encontrado", e);
         }
     }
 }
-
-
